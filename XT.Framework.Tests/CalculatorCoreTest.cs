485fb061e3d3b98d7a7ca575671a2d789a135ab8
Tomson Xu <tomsonxu@um.cityu.edu.hk>
2014-11-19 11:03:31 +0800

    init version

---
+++ XT.Framework.Tests/CalculatorCoreTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XT.Calculator.Framework;
+
+namespace XT.Framework.Tests
+{
+    [TestClass]
+    public class CalculatorCoreTest
+    {
+        [TestMethod]
+        public void Test_CalculatorCore_Add()
+        {
+            CalculatorCore cal = new CalculatorCore();
+            int result = cal.add(1, 2);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
